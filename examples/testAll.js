dedf2920f1f29f92957035f7281d09c9a5b71d6b
Thai Pangsakulyanont @ Taskworld <thai.p@taskworld.com>
2016-12-19 14:00:24 +0700

    :sparkles: Add a script to test all

---
+++ examples/testAll.js
@@ -0,0 +1,21 @@
+// Checks that all examples are working.
+const glob = require('glob')
+const chalk = require('chalk')
+let failures = 0
+
+console.log('Checking if all examples work!')
+
+for (const file of glob.sync('examples/*/tests/**/*.js')) {
+  try {
+    require('child_process').execFileSync('./bin/prescript', [ file ])
+    console.log(chalk.bgGreen.bold(' OK '), file)
+  } catch (e) {
+    console.log(chalk.bgRed.bold(' NG '), file)
+    console.log(e.output.join(''))
+    failures += 1
+  }
+}
+
+if (failures) {
+  process.exit(1)
+}
