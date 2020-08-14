d945b5da84677ae21630b5f2737bf42705c5d567
Thai Pangsakulyanont <thai.p@taskworld.com>
2020-08-14 09:11:01 +0000

    Add failing test for #27

--- examples/regression/tests/Issue27.js
+++ examples/regression/tests/Issue27.js
@@ -1,14 +1,17 @@
 const { action } = require('../../..')
 const assert = require('assert')
+const fs = require('fs')
+const glob = require('glob')
+const { execFileSync } = require('child_process')
 
 action('Run test (it should fail)', async () => {
   let failed = false
-  require('child_process').execFileSync('rm', [
+  execFileSync('rm', [
     '-rf',
     'tmp/issue27-allure-results'
   ])
   try {
-    require('child_process').execFileSync(
+    execFileSync(
       './bin/prescript',
       [require.resolve('../fixtures/Issue27-AnsiColorTestFixture.js')],
       {
@@ -23,5 +26,23 @@ action('Run test (it should fail)', async () => {
   } catch (error) {
     failed = true
   }
-  assert(failed)
+  assert(failed, 'Expected prescript command to fail')
+})
+
+action('Verify that there is an XML allure result generated', async () => {
+  const files = glob.sync('*.xml', { cwd: 'tmp/issue27-allure-results' })
+  assert(files.length > 0, 'Expected to file XML files')
+})
+
+action('Generate an allure-report', async () => {
+  execFileSync(
+    'yarn',
+    ['allure', 'generate', '--clean', ...['--output', 'tmp/issue27-allure-report'], 'tmp/issue27-allure-results']
+  )
+  assert(fs.existsSync('tmp/issue27-allure-report'), 'Expected report to exist')
+})
+
+action('Verify that there is a test case file', async () => {
+  const files = glob.sync('*.json', { cwd: 'tmp/issue27-allure-report/data/test-cases' })
+  assert(files.length > 0, 'Expected test case JSON files to exist')
 })
